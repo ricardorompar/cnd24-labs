a1ff2c3335183d6ff243cc954457933d6300844b
ricardorompar <ricardorompar@hotmail.com>
2024-06-06 19:34:38 +0000

    changed to new version of py and dockerfile

--- hello.py
+++ hello.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-return 'Hello World!'
+  return 'Hello World!'
 
 if __name__ == '__main__':
-app.run(host='0.0.0.0', port=8080)
+  port = int(os.environ.get('PORT', 8080))
+  app.run(host='0.0.0.0', port=port)
